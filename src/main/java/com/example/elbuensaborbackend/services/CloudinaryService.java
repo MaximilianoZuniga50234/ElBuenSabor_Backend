8a56ae709efeb7afbee974de9a4e0dc77ed6ad45
MaximilianoZuniga50234 <maximiliano.zuniga@alumnos.frm.utn.edu.ar>
2023-08-25 21:00:09 -0300

    implemented cloudinary connection

---
+++ src/main/java/com/example/elbuensaborbackend/services/CloudinaryService.java
@@ -0,0 +1,47 @@
+package com.example.elbuensaborbackend.services;
+
+import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class CloudinaryService {
+
+    Cloudinary cloudinary;
+
+    private Map<String, String> valuesMap = new HashMap<>();
+
+    public CloudinaryService(){
+        valuesMap.put("cloud_name", "dycogxoko");
+        valuesMap.put("api_key", "755738381293712");
+        valuesMap.put("api_secret", "keCyEZxeLQ-cJEseiAWMbuB6Tt4");
+        cloudinary = new Cloudinary(valuesMap);
+    }
+
+    public Map upload(MultipartFile multipartFile) throws IOException {
+        File file = convert(multipartFile);
+        Map result = cloudinary.uploader().upload(file, ObjectUtils.emptyMap());
+        file.delete();
+        return result;
+    }
+
+    public Map delete(String id) throws IOException {
+        Map result = cloudinary.uploader().destroy(id, ObjectUtils.emptyMap());
+        return result;
+    }
+
+    private File convert(MultipartFile multipartFile) throws IOException {
+        File file = new File(multipartFile.getOriginalFilename());
+        FileOutputStream fileOutputStream = new FileOutputStream(file);
+        fileOutputStream.write(multipartFile.getBytes());
+        fileOutputStream.close();
+        return file;
+    }
+}
